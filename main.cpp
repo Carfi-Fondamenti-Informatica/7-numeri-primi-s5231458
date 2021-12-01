183bb8010ee6761851234fb0d75f672b9d028b85
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-12-01 13:45:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int n=0;
+    cin>>n;
+    if (nprimo(n, n-1)){
+        cout<<"numerp primo";
 
-int main(){
-  return 0;
+    }else{
+        cout<<"numero non primo";
+    }
+
+    return 0;
 }
