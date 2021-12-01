31d9fc7b3086bfced7e3cc75cf140ea50838c3b3
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-12-01 13:44:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+bool nprimo (int n, int a){
+    if(n<0){
+        return false;
+    }else if (a==1){
+        return true;
 
+    }else if (n%a==0){
+        return false;
+    }
+    nprimo (n, --a);
+}
